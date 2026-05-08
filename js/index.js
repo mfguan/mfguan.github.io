ceb56775e431ff7b246167a810623d4ca39add41
mfguan <mengfeiguan@gmail.com>
2026-05-07 23:15:33 -0500

    0507 revamp

--- js/index.js
+++ js/index.js
@@ -17,5 +17,25 @@ function reveal() {
   }
 }
 
-window.addEventListener("scroll", reveal); 
+window.addEventListener("scroll", reveal);
+
+document.getElementById("site-footer").innerHTML = `
+<section class="footer body-s">
+    <div class="footer-flex-container width">
+        <div>
+            <br class="mobile">
+            <p>Site hand-crafted and hosted by GitHub.</p>
+            <p>© 2026. All rights reserved.</p>
+        </div>
+        <div class="footer-left-flex-container">
+            <p>Let's connect!</p>
+            <div class="icon">
+                <a href="https://scholar.google.com/citations?user=_48_CFsAAAAJ&hl=en" target="_blank"><img src="images/icon-gs-default.png" class="img-icon"></a>
+                <a href="https://www.linkedin.com/in/mengfeiguan/" target="_blank"><img src="images/icon-li-default.png" class="img-icon"></a>
+                <a href="mailto:mengfeiguan@gmail.com"><img src="images/icon-em-default.png" class="img-icon"></a>
+            </div>
+        </div>
+    </div>
+</section>
+`;
 
